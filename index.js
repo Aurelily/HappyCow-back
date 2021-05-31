01d1422c9e92617ccc119654cb14cfcf210d0110
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-05-31 18:21:55 +0200

    tentative filtre

--- index.js
+++ index.js
@@ -24,7 +24,37 @@ app.use(userRoutes);
 
 //Welcome route for the app !
 app.get("/", (req, res) => {
-  res.status(200).json(data);
+  res.status(200).json("welcome to HappyCow API !");
+});
+
+//GET : all restaurants or Query type
+app.get("/restaurants", async (req, res) => {
+  try {
+    //Définir les différentes sortes de requetes/recherches possibles
+    const reqType1 = req.query.type1;
+
+    // //Je crée un objet vide qui contiendra mes clés de filtre
+    // let filters = {};
+
+    // //si il y a un type j'ajoute la clé type1 à mon objet filters'
+    // if (reqType1) {
+    //   filters.type1 = new RegExp(reqType1, "i");
+    // }
+    // //Je mets mon objet filters dans ma requete find() pour obtenir mon résultat filtré
+    // let restaurants = await data.find(filters);
+
+    // // calculer le nombre de résultats
+    // const count = await data.countDocuments(filters);
+
+    // //réponse au client
+    // res.status(200).json({
+    //   count: count,
+    //   restaurants: restaurants,
+    // });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //urls to connect to database
