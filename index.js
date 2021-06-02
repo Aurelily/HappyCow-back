74f459e13e8ce03cbc9603ddfc633516940681db
Aurélie PREAUD <aureliepreaud@MacBook-Air-de-Aurelie.local>
2021-06-02 12:23:21 +0200

    tentative search bar 1

--- index.js
+++ index.js
@@ -32,25 +32,56 @@ app.get("/restaurants", async (req, res) => {
   try {
     //Définir les différentes sortes de requetes/recherches possibles
     const reqType = req.query.types;
+    const reqName = req.query.name;
+
+    //Fonction de tri ordre alphabetique par nom
+    function tri(a, b) {
+      return a.name > b.name ? 1 : -1;
+    }
 
     //Je crée un tableau vide qui contiendra nouveaux résultats filtrés selon la query
     let newResult = [];
 
     //si il y a un type en query je modifie les datas envoyées
-    if (reqType) {
+    if (reqType || reqName) {
       //Je récupère dans un tableau ma query contenant plusieurs types séparés par des virgules
       const typesTab = reqType.split(",");
-      console.log("typesTab:" + typesTab);
-      console.log("reqType:" + reqType);
+
+      //Je récupère le test regex de ma recherche
+      let reqSearch = new RegExp(reqName, "i");
+
+      //Je push les restaus qui correspondent uniquement aux query récupérés dans typesTab
       for (let i = 0; i < data.length; i++) {
-        if (data[i].type.indexOf(reqType) !== -1) {
+        for (let j = 0; j < typesTab.length; j++) {
+          if (data[i].type.indexOf(typesTab[j]) !== -1) {
+            newResult.push(data[i]);
+          }
+        }
+        //Je push les restaus qui correspondent à ma recherche par name
+        if (data[i].name.indexOf(reqSearch) !== -1) {
           newResult.push(data[i]);
         }
       }
+      newResult.sort(tri);
       res.status(200).json(newResult);
     } else {
+      data.sort(tri);
       res.status(200).json(data);
     }
+
+    // if (reqName) {
+    //   let reqSearch = new RegExp(reqName, "i");
+    //   for (let k = 0; k < data.length; k++) {
+    //     if (data[k].name.indexOf(reqSearch) !== -1) {
+    //       newResult.push(data[k]);
+    //     }
+    //   }
+    //   newResult.sort(tri);
+    //   res.status(200).json(newResult);
+    // } else {
+    //   data.sort(tri);
+    //   res.status(200).json(data);
+    // }
 
     // res.status(200).json(data);
   } catch (error) {
